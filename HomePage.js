c41faf7cbd2b00145cec91e94e5dcb11b613301d
uneeb123 <uneeb.i.am@gmail.com>
2018-03-16 17:50:30 -0700

    Transactions panel appears

--- HomePage.js
+++ HomePage.js
@@ -5,14 +5,54 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  FlatList
 } from 'react-native';
+
+class Transaction extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.transaction_container}>
+        <View style={styles.date_container}>
+          <Text>{this.props.item.date}</Text>
+        </View>
+        <View style={styles.addr_container}>
+          <Text>{this.props.item.addr}</Text>
+        </View>
+        <View style={styles.prince_container}>
+          <Text>{this.props.item.amount_bitcoin + "\n"}</Text>
+          <Text>{"$" + this.props.item.amount_dollar}</Text>
+        </View>
+      </View>
+    );
+  }
+}
 
 class TransactionList extends Component<{}> {
   render() {
     return (
       <View style={styles.transactions}>
-        <Text>Placeholder</Text>
+        <FlatList 
+          data={[
+            {
+              key: '1',
+              date: '03/13/2018',
+              addr: '1BvBMSEYstWetqTFn5Au4m4GFg7xJaNVN2',
+              amount_bitcoin: '0.05',
+              amount_dollar: '80.0',
+              incoming: true
+            },
+            {
+              key: '2',
+              date: '03/16/2018',
+              addr: '3J98t1WpEZ73CNmQviecrnyiWrnqRhWNLy',
+              amount_bitcoin: '0.02',
+              amount_dollar: '35.0',
+              incoming: false
+            }
+          ]}
+          renderItem={({item}) => <Transaction item={item}/>}
+        />
       </View>
     );
   }
@@ -49,18 +89,31 @@ export default class HomePage extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: 'stretch',
     backgroundColor: '#F5FCFF',
+    padding: 5,
   },
   transactions: {
     flex: 1,
-    height: 100,
   },
   bottomButtons: {
-    alignSelf: 'stretch',
-    alignItems: 'center',
     justifyContent: 'space-between',
     flexDirection: 'row',
     margin: 2,
   },
+  transaction_container: {
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    margin: 2,
+    elevation: 2,
+  },
+  date_container: {
+    flex: 1,
+  },
+  addr_container: {
+    flex: 2,
+  },
+  price_container: {
+    flex: 1,
+  }
 });
