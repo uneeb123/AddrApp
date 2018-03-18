e45cead13084f88871c52b28500ba8e057748ee6
uneeb123 <uneeb.i.am@gmail.com>
2018-03-18 11:10:52 -0700

    Info page appears

--- InfoPage.js
+++ InfoPage.js
@@ -6,8 +6,9 @@ import { Text, View, Button } from 'react-native';
 export default class InfoPage extends Component<{}> {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>This is a modal!</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#93979e' }}>
+        <Text style={{fontSize: 20, textAlign: 'center', color: 'white'}}>Developed by Nick Dimitrov and Uneeb Agha. We are based in Seattle!</Text>
+        <View style={{height:100}}/>
         <Button
           onPress={() => this.props.navigation.goBack()}
           title="Dismiss"
